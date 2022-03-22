8e70520836d5c4939eb71d9f393d2b6f5e3c5d29
Sergey Danilov <shyundaid@gmail.com>
2022-03-22 18:23:29 +0300

    works inciorrectly

--- MatrixMultiplic/MatrixMultiplic.cpp
+++ MatrixMultiplic/MatrixMultiplic.cpp
@@ -1,4 +1,4 @@
-﻿#define N 16
+﻿#define N 800
 
 #include <stdlib.h>
 #include <iostream>
@@ -17,6 +17,7 @@ MPI_Comm row_comm;
 MPI_Comm col_comm;
 
 int* matrixA;
+int* matrixAt;
 int* matrixB;
 int* matrixC;
 
@@ -28,16 +29,16 @@ void multiply_matrix() {
 	for (int i = 0; i < submatrix_size; i++) {
 		for (int j = 0; j < submatrix_size; j++) {
 			for (int k = 0; k < submatrix_size; k++) {
-				matrixC[i * submatrix_size + j] += matrixA[i * submatrix_size + k] * matrixB[j + k * submatrix_size];
+				matrixC[i * submatrix_size + j] += matrixAt[i * submatrix_size + k] * matrixB[j + k * submatrix_size];
 			}
 		}
 	}
 }
 
-void print_matrix() {
-	for (int i = 0; i < submatrix_size; i++) {
-		for (int j = 0; j < submatrix_size; j++) {
-			std::cout << matrixC[i * submatrix_size + j] << " ";
+void print_matrix(int s, int* matrix) {
+	for (int i = 0; i < s; i++) {
+		for (int j = 0; j < s; j++) {
+			std::cout << matrix[i * s + j] << " ";
 		}
 		std::cout << std::endl;
 	}
@@ -53,6 +54,16 @@ void prepare_block(int a, int b, int* matrix, int* matrixOriginal) {
 	}
 }
 
+void populate_block(int a, int b, int* matrix, int* matrixOriginal) {
+	long long index = 0;
+	for (int i = 0; i < submatrix_size; i++) {
+		for (int j = 0; j < submatrix_size; j++) {
+			matrixOriginal[(a*submatrix_size+i) * N + (b*submatrix_size+j)] = matrix[index];
+			index++;
+		}
+	}
+}
+
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -60,7 +71,7 @@ int main(int argc, char** argv) {
 	if (!((size == 1) || (size == 2) || (size == 4) || (size == 9) || (size == 16))) {
 		MPI_Abort(MPI_COMM_WORLD, 1);
 	}
-
+	double start = MPI_Wtime();
 	ndim = (int)sqrt(size);
 	ndims[0] = ndims[1] = ndim;
 	submatrix_size = N / ndim;
@@ -74,6 +85,7 @@ int main(int argc, char** argv) {
 	MPI_Cart_coords(com, rank, 2, coords);	
 	//prepare data
 	if (rank == 0) {
+
 		matrixAoriginal = new int[N * N];
 		matrixBoriginal = new int[N * N];
 		matrixCoriginal = new int[N * N];
@@ -88,17 +100,18 @@ int main(int argc, char** argv) {
 	}
 
 	matrixA = new int[submatrix_size * submatrix_size];
+	matrixAt = new int[submatrix_size * submatrix_size];
 	matrixB = new int[submatrix_size * submatrix_size];
 	matrixC = new int[submatrix_size * submatrix_size];
 	for (int i = 0; i < submatrix_size; i++) {
 		for (int j = 0; j < submatrix_size; j++) {
-			matrixC[i * submatrix_size + j] = 0;
+			matrixC[i * submatrix_size + j] = 8;
 			matrixB[i * submatrix_size + j] = 0;
 			matrixA[i * submatrix_size + j] = 0;
+			matrixAt[i * submatrix_size + j] = 0;
 		}
 	}
 	MPI_Status mStatus;
-	std::cout << "resay";
 
 	// A B distribution for all processes
 	if (rank == 0) {
@@ -107,65 +120,55 @@ int main(int argc, char** argv) {
 				if (i + j == 0) continue;
 				prepare_block(i, j, matrixA, matrixAoriginal);
 				prepare_block(i, j, matrixB, matrixBoriginal);
-				MPI_Send(matrixA, submatrix_size * submatrix_size, MPI_DOUBLE, i * ndim + j, 1, com);
-				MPI_Send(matrixB, submatrix_size * submatrix_size, MPI_DOUBLE, i * ndim + j, 1, com);
+				MPI_Send(matrixA, submatrix_size * submatrix_size, MPI_INT, i * ndim + j, 1, com);
+				MPI_Send(matrixB, submatrix_size * submatrix_size, MPI_INT, i * ndim + j, 1, com);
 			}
 		}
-		std::cout << "sent blocks" << std::endl;
 		prepare_block(0, 0, matrixA, matrixAoriginal);
 		prepare_block(0, 0, matrixB, matrixBoriginal);
-
-		// HERe local mult for 0 process
-
-		for (int i = 0; i < ndim; i++) {
-			for (int j = 0; j < ndim; j++) {
-				if (i + j == 0) continue;
-				MPI_Recv(matrixC, submatrix_size * submatrix_size, MPI_DOUBLE, i * ndim + j, 1, com, &mStatus);
-				// here some functions for result
-			}
-		}
-
-		std::cout << "finalize" << std::endl;
-		delete[] matrixAoriginal;
-		delete[] matrixBoriginal;
-		delete[] matrixCoriginal;
-	}
-	else {
-		MPI_Recv(matrixA, submatrix_size * submatrix_size, MPI_DOUBLE, 0, 1, com, &mStatus);
-		MPI_Recv(matrixB, submatrix_size * submatrix_size, MPI_DOUBLE, 0, 1, com, &mStatus);
-		// HERE Your LOGIc for multiplication and resending
-		MPI_Send(matrixC, submatrix_size * submatrix_size, MPI_DOUBLE, 0, 1, com);
 	}
 
+	if (rank != 0) {
+		MPI_Recv(matrixA, submatrix_size * submatrix_size, MPI_INT, 0, 1, com, &mStatus);
+		MPI_Recv(matrixB, submatrix_size * submatrix_size, MPI_INT, 0, 1, com, &mStatus);
+	}
 	
-
-	// main loop 
-	/*
 	for (int iter = 0; iter < ndim; iter++) {
-		// A` distribution
-		if (coords[1] == (coords[0] + iter) % ndim) {
-			MPI_Bcast(matrixA, submatrix_size * submatrix_size, MPI_DOUBLE, coords[1], row_comm);
-		}
-
-		// multiply matrix
+		memcpy(matrixAt, matrixA, submatrix_size * submatrix_size);
+		MPI_Bcast(matrixAt, submatrix_size * submatrix_size, MPI_INT, (coords[0] + iter)%ndim, row_comm);
+		
 		multiply_matrix();
 
-		// B` distribution
-		int next = (coords[0] + 1)%(ndim - 1);
+		int next = (coords[0] + 1);
+		if (coords[0] == ndim-1) {
+			next = 0;
+		}
 		int prev = (coords[0] - 1);
 		if (coords[0] == 0) {
 			prev = ndim - 1;
 		}
 		MPI_Sendrecv_replace(matrixB, submatrix_size * submatrix_size, MPI_DOUBLE, next, 0, prev, 0, col_comm, &mStatus);
+		
 	}
 	
+	if (rank != 0) {
+		MPI_Send(matrixC, submatrix_size * submatrix_size, MPI_INT, 0, 1, com);
+	}
 
+	if (rank == 0) {
+		populate_block(0, 0, matrixC, matrixCoriginal);
+		for (int i = 0; i < ndim; i++) {
+			for (int j = 0; j < ndim; j++) {
+				if (i + j == 0) continue;
+				MPI_Recv(matrixC, submatrix_size * submatrix_size, MPI_INT, i * ndim + j, 1, com, &mStatus);
+				populate_block(i, j, matrixC, matrixCoriginal);
+			}
+		}
+		delete[] matrixCoriginal;
+		double end = MPI_Wtime();
+		printf("%f", end - start);
+	}
 
-
-	delete[] matrixA;
-	delete[] matrixB;
-	delete[] matrixC;
-	*/
 	MPI_Finalize();
 	return 0;
 }
